787870f61022d774793f148aa0cf9b612c3c5baf
Mohamed Ayaanle <40414487@live.napier.ac.uk>
2020-02-06 09:34:50 -0800

    corrected queryfile errors and OutputUI is functioning.

---
+++ src/main/java/com/napier/sem/country.java
@@ -0,0 +1,50 @@
+package com.napier.sem;
+
+
+public class country {
+
+    public String Code;
+
+    public String Name;
+
+
+    public String Continent;
+
+
+    public String Region;
+
+
+    public Double SurfaceArea;
+
+
+    public int IndepYear;
+
+
+    public double Population;
+
+
+    public double LifeExpectancy;
+
+
+    public double GNP;
+
+
+    public double GNPOld;
+
+
+    public String LocalName;
+
+
+    public String GovernmentForm;
+
+
+    public String HeadOfState;
+
+
+    public int Capital;
+
+
+    public String Code2;
+
+
+}
