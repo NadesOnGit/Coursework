9eb662b84f89d36a4c0ca8e2b44c112dc1b1b697
Jack Masterton <jack34574@gmail.com>
2020-03-09 13:29:33 +0000

    Changed to from table to command line interface

--- src/main/java/com/napier/sem/OutputLinker.java
+++ src/main/java/com/napier/sem/OutputLinker.java
@@ -10,48 +10,7 @@ import java.util.ArrayList;
 
 public class OutputLinker {
 
-    //Used to create a table that is based on the Country object
-    public void countryTable(ArrayList<Country> inputList, boolean doubleTable) {
-        OutputUIclass outputUI = new OutputUIclass();
-        String[][] string = new String[inputList.size()][4];
-        for (int i = 0; i < inputList.size(); i++) {
-            string[i][0] = inputList.get(i).Code;
-            string[i][1] = inputList.get(i).Name;
-            string[i][2] = inputList.get(i).Region;
-            string[i][3] = Double.toString(inputList.get(i).Population);
-            string[i][4] = Integer.toString(inputList.get(i).Capital);
-        }
-        if (doubleTable) {
-            //outputUI.tableDouble(inputList,);
-        } else {
-            outputUI.tableSingle(string);
-        }
-
-
-    }
-
-    //Used to create a table that is based on the City object
-    public void cityTable(ArrayList<City> inputList, boolean doubleTable) {
-        OutputUIclass outputUI = new OutputUIclass();
-        String[][] string = new String[inputList.size()][3];
-        for (int i = 0; i < inputList.size(); i++) {
-            string[i][0] = inputList.get(i).Name;
-            string[i][1] = inputList.get(i).Country;
-            string[i][2] = inputList.get(i).District;
-            string[i][3] = Integer.toString(inputList.get(i).Population);
-        }
-        if (doubleTable) {
-            //outputUI.tableDouble(inputList,);
-        } else {
-            outputUI.tableSingle(string);
-        }
-    }
-
-    public void extraTable() {
-
-    }
-
-
+    
     //used to process the output data from a query to make it readable to the table builder
     public boolean SQLinititate(String input, String countryInput, String distInput, String contInput, String regInput, String numbInput) {
         QueryHandler query = new QueryHandler("world.sql");
@@ -61,15 +20,9 @@ public class OutputLinker {
             if (input == "All the countries in the world organised by largest population to smallest.") {
 
                 ArrayList<Country> list = query.popWorld();
-                String war = "it broken :(";
-                String[][] string = new String[list.size()][4];
-                for (int i = 0; i < list.size(); i++) {
-                    string[i][0] = list.get(i).Name;
-                    string[i][1] = list.get(i).Continent;
-                    string[i][2] = list.get(i).Region;
-                    string[i][3] = Double.toString(list.get(i).Population);
-                    string[i][4] = Integer.toString(list.get(i).Capital);
-                    System.out.format("%-18s%-8s%-8s",string[i][0], string[i][1], string[i][2], string[i][3], string[i][4]);
+                System.out.println("All countries in the world organised by population from largest to smallest");
+                for (Country country : list) {
+                    System.out.format("%-18s%-8s%-8s", country.Name, country.Continent, country.Region, country.Population, country.Capital);
                 }
 
             } else if (input == "All the countries in a continent organised by largest population to smallest.") {
