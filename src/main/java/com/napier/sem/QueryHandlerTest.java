65281eb9ec8c2e5c9b8e8cee2d06797c41deee65
Mohamed Ayaanle <40414487@live.napier.ac.uk>
2020-03-16 02:45:21 -0700

    working on testing

--- src/main/java/com/napier/sem/QueryHandlerTest.java
+++ src/main/java/com/napier/sem/QueryHandlerTest.java
@@ -1,157 +1,20 @@
 package com.napier.sem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueryHandlerTest {
+    QueryHandler queryHandler = new QueryHandler("sadfsdf");
 
-    @org.junit.jupiter.api.Test
-    void getErrorMessage() {
-
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void getContinents() {
-
-
+        assertEquals(queryHandler.getContinents().get(0), "North America");
     }
 
-    @org.junit.jupiter.api.Test
-    void getRegions() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getCountries() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void popWorld() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void popContinent() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void popRegion() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nWorldCountryPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nContinentCountryPop() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void nRegionCountryPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void worldCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void continentCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void regionCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void countryCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void districtCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nWorldCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nContinentCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nRegionCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nCountryCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nDistrictCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void worldCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void continentCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void regionCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nWorldCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nContinentCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void nRegionCapitalCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void continentInOutCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void regionInOutCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void countryInOutCityPop() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void languageReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void worldPopulationReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void continentPopulationReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void regionPopulationReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void countryPopulationReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void districtPopulationReport() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void cityPopulationReport() {
+        assertEquals(5, 5);
     }
 }
